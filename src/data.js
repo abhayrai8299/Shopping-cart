503722630fb61a4c87cafaa0de874f94839f5eb6
Abhay Rai <abhay.rai8299@gmail.com>
2022-06-20 09:13:26 +0530

    shopping-cart

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data={
+    products:[
+    {
+     id:'1',
+     name:'T-Shirt',
+     price:500,
+     image:'https://5.imimg.com/data5/YX/OO/TA/ANDROID-108727015/product-jpeg-500x500.jpg',
+    },
+    {id:'2',
+    name:'Mobile',
+    price:14000,
+    image:'https://i.gadgets360cdn.com/products/large/motorola-edge-30-pro-657x800-1645713862.jpg',
+    },
+    {id:'3',
+     name:'Laptop',
+     price:25000,
+     image:'https://images.prismic.io/frameworkmarketplace/cca31de3-3b75-4932-af96-7646b7eba6c7__DSC3630-Edit-cropped.jpg?auto=compress,format',
+    },
+    ],
+};
+
+export default data;
